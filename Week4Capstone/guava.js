76c2678bf3f3015e2be0fdfc0270228aeb677b9f
Anthony <anth0ny0153@gmail.com>
2018-05-22 15:24:35 -0400

    first commit for week 4 capstone

---
+++ Week4Capstone/guava.js
@@ -0,0 +1,24 @@
+﻿var cart = [];
+var cartItems = [];
+var productName = ["Apple", "Taco", "Sushi", "Water", "Cheese", "Watermelon"];
+var productPrice = [1.00, 2.00, 4.25, 2.50, 1.50, 7.00];
+function AddToCart(i) {
+    cart.push(productPrice[i]);
+    cartItems.push(productName[i]);
+}
+function getTotal() {
+    var total = 0;
+    for (var i = 0; i < cart.length; i++)
+    {
+        total += cart[i];
+    }
+    console.log("Your total is: $" + total);
+
+}
+function displayCart() {
+    console.clear();
+    for (var i = 0; i < cart.length; i++) {
+        console.log(cartItems[i]);
+    }
+    getTotal();
+}
